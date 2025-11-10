0be4e2654692c77e17917e19e64a105a9357910a
Noah Coyner <noahcoyner70@gmail.com>
2025-11-09 19:25:18 -0500

    template for CodeForces

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+class Solution {
+
+    public:
+    void solve() {
+
+    };
+};
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    Solution obj;
+    obj.solve();
+}
