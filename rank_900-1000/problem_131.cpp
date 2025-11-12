e2e41b73de07fec8e74cc092f2b5708314a75e0c
Coyn123 <61031960+Coyn123@users.noreply.github.com>
2025-11-12 18:15:14 -0500

    Trasnfering problem

---
+++ rank_900-1000/problem_131.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Solution {
+
+    public:
+    void solve() {
+      std::string input;
+      std::cin << input;
+      // Check if first is lower AND rest is upper
+      // If not, is it all upper?
+      // if not -> return the same string? (nothing specified otherwise)
+      // if is -> fix and return string (Xxx) not->(xXX) or (XXX)
+
+    };
+};
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    Solution obj;
+    obj.solve();
+}
